955d6280ae210dc2023de8718af5ba97d2c66f9e
Hoy Cheung <hoycch@gmail.com>
2024-04-23 15:13:59 +0100

    create PricingRule and SpecialPrice classes

---
+++ PricingRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketCheckout
+{
+    public class PricingRule
+    {
+        public string SKU { get; }
+        public int UnitPrice { get; }
+        public SpecialPrice SpecialPrice { get; }
+
+        public PricingRule(string sku, int unitPrice, SpecialPrice specialPrice = null)
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+            SpecialPrice = specialPrice;
+        }
+    }
+}
